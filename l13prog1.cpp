a8afca23a14b59a456e1f37ded4a441e23d8a9a4
Oleksandr-yalovskyi <77542534+Oleksandr-yalovskyi@users.noreply.github.com>
2021-01-16 22:38:57 +0200

    Update l13prog1.cpp

--- l13prog1.cpp
+++ l13prog1.cpp
@@ -9,6 +9,25 @@ int min(double a, double b) {
 	}
 }
 
+void arrayPrint(float array[], int size) {
+    for ( int i = 0; i < size; i++ ) {
+    cout << array[i] << " ";
+    }
+}
+
+void sort(float array[], int size) {
+    for ( int i = size; i > 0; i-- ) {
+        for ( int j = 1; j < i; j++ ) {
+            if ( array[j-1] > array[j] ) {
+                float buf = array[j-1];
+
+                array[j-1] = array[j];
+                array[j] = buf;
+            }
+        }
+    }
+}
+
 int main() {
 	float A[10], B[10], C[10];
 	float *a = A;
@@ -18,11 +37,10 @@ int main() {
 	for ( int i = 0; i < 10; i++ ) {
 		*(a+i) = rand() % 6 - 2.5;
 		*(b+i) = rand() % 5 - 2.5;
-		cout << *(a+i) << " ";
 	}
+
 	for ( int i = 0, j = 9; i < 10, j >= 0; i++, j-- ) {
 		*(c+i) = min(*(a+i), *(b+j));
-		//cout << *(c+i) << " ";
 	}
 
 	float maxA = *(a+0);
@@ -56,11 +74,25 @@ int main() {
         }
 	}
 
-	cout << endl;
-
-	cout << "Max negative in A: " << maxA << endl;
+    cout << "Max negative in A: " << maxA << endl;
 	cout << "Max negative in B: " << maxB << endl;
 	cout << "Max negative in C: " << maxC << endl;
 
+    sort(A, 10);
+    sort(B, 10);
+    sort(C, 10);
+
+    cout << "Array A: ";
+    arrayPrint(A, 10);
+    cout << endl;
+
+    cout << "Array B: ";
+    arrayPrint(B, 10);
+    cout << endl;
+
+    cout << "Array C: ";
+    arrayPrint(C, 10);
+    cout << endl;
+	
 	return 0;
 }
