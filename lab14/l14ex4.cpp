6c4625a8cfe1f0cddbc757737a63db5b238a2126
Oleksandr-yalovskyi <77542534+Oleksandr-yalovskyi@users.noreply.github.com>
2021-02-17 18:28:13 +0200

    Create l14ex4.cpp

---
+++ lab14/l14ex4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+inline int Double(int);
+
+int main(){
+	int target;
+
+	cout << "Enter a number : ";
+	cin >> target;
+	cout << " \n";
+	target = Double(target);
+	cout << " Target " <<  target <<  endl;
+	target = Double(target);
+	cout << " Target "<<  target <<  endl;
+	target = Double(target);
+	cout << " Target "<<  target <<  endl;
+	return 0;
+}
+
+int Double(int target){
+	return 2*target;
+}
