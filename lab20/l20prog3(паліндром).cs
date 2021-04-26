d67d9d90149e5c0123d50ed0656f2f635bb63fa9
Oleksandr-yalovskyi <77542534+Oleksandr-yalovskyi@users.noreply.github.com>
2021-04-26 15:47:45 +0300

    Create l20prog3(паліндром).cs

---
+++ lab20/l20prog3(паліндром).cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace l20{
+    class Program{
+        static void Main(string[] args){
+	        string input = Console.ReadLine();
+			string output;
+
+	        string s = input.Replace(" ", string.Empty);
+	        s = s.ToLower();
+	        int LastIndex = s.Length - 1;
+	        for ( int i = 0; i < s.Length / 2; i++ ) {
+	        	if ( s[i] != s[LastIndex - i] ) {
+	        		Console.WriteLine("String isn't a palindrom");
+	        	} else {
+	        		Console.WriteLine("String is a palindrom");
+	        	}
+	        }
+		}
+    }
+}
