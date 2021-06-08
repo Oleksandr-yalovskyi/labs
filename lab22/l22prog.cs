8d5ae923039d36af00445e4afb59e307fdec278c
Oleksandr-yalovskyi <77542534+Oleksandr-yalovskyi@users.noreply.github.com>
2021-06-08 09:43:15 +0300

    Create l22prog.cs

---
+++ lab22/l22prog.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace l22{
+    class Program{
+        private static void FillRandArray(int[] mas) {
+            Random rand = new Random();
+            for ( int i = 0; i < mas.Length; i++ ) {
+                mas[i] = rand.Next(0, 101);
+            }
+        }
+        private static void PrintArray(int[] mas) {
+            for ( int i = 0; i < mas.Length; i++ )
+                Console.Write($"{mas[i]} ");
+            Console.WriteLine();
+        }
+        static void CocktailSort(int size, int[] array){
+            for (int i = 0; i < size / 2; i++){
+                bool swapped = false;
+                for (int j = i; j < size - i - 1; j++){
+                    if (array[j] < array[j + 1]){
+                        int tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                        swapped = true;
+                    }
+                }
+                for (int j = size - 2 - i; j > i; j--){
+                    if (array[j] > array[j - 1]){
+                        int tmp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = tmp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped) { break; }
+            }
+        }
+        static void Main(string[] args){
+            Console.Write("Enter size of array: ");
+            try{
+                int size = Convert.ToInt32(Console.ReadLine());
+                int[] array = new int[size];
+
+                Console.Write("Array before sorting: ");
+                FillRandArray(array);
+                PrintArray(array);
+                CocktailSort(size, array);
+                Console.Write("Array after sorting: ");
+                PrintArray(array);
+            }
+            catch {
+                Console.WriteLine("Incorrect size entered");
+            }
+        }
+    }
+}
